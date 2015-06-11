fc0ef49b4379a0d84bcfa229d741a757c6d2fdba
reysonafonso <reysonafonso1979@gmail.com>
2015-06-11 12:58:20 -0400

    Arquivo Insert SQL
    Fase 3 Arquivo INSERT usuarios SQL

---
+++ insert_usuarios.sql
@@ -0,0 +1,19 @@
+INSERT INTO `school`.`usuarios`
+(`usuario`,
+`email`,
+`senha`)
+VALUES
+('reysonafonso',
+'reyson@ufam.edu.br',
+'osnofinha1308'
+);
+
+INSERT INTO `school`.`usuarios`
+(`usuario`,
+`email`,
+`senha`)
+VALUES
+('wesleywilliams',
+'wesleywilliams@son.com',
+'12345'
+);
